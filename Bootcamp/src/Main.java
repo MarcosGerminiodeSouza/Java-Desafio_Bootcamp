8d0227c726709f3a303876c4cc7ad34b861be56c
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2023-02-06 16:06:03 -0300

    Update Main

--- Bootcamp/src/Main.java
+++ Bootcamp/src/Main.java
@@ -1,5 +1,5 @@
 import java.time.LocalDate;
-
+//import componentes.Conteudo;
 import componentes.Curso;
 import componentes.Mentoria;
 
@@ -7,17 +7,17 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Curso curso1 = new Curso();
         curso1.setTitulo("Curso Java");
-        curso1.setDescricao("Descriçao curso Java");
+        curso1.setDescricao("descriçao do Curso Java");
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
         curso2.setTitulo("Curso JS");
-        curso2.setDescricao("Descriçao curso JS");
-        curso2.setCargaHoraria(4);
+        curso2.setDescricao("descriçao do Curso JS");
+        curso2.setCargaHoraria(4);        
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria Java");
-        mentoria.setDescricao("Descriçao mentoria Java");
+        mentoria.setDescricao("Mentoria descriçao");
         mentoria.setData(LocalDate.now());
 
         System.out.println(curso1);
