3888962a6a5027e441f7799b1a86e1d01e671d91
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2023-02-06 14:58:04 -0300

    Add Mentoria

--- Bootcamp/src/componentes/Mentoria.java
+++ Bootcamp/src/componentes/Mentoria.java
@@ -6,28 +6,37 @@ public class Mentoria {
     private String titulo;
     private String descricao;
     private LocalDate data;
-    //titulo
+
+    //Mentoria titulo
     public String getTitulo() {
         return titulo;
     }
-
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-    //descricao
+
+    //Mentoria descricao
     public String getDescricao() {
         return descricao;
     }
-
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
-    //data
+
+    //Mentoria data
     public LocalDate getData() {
         return data;
     }
-
     public void setData(LocalDate data) {
         this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
     }
 }
